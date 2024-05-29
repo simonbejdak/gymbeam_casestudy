c60fc851163a457c2512745deda4f2d513be0c50
simonbejdak <bejdakxd@gmail.com>
2024-05-29 17:48:44 +0200

    GymBeam Case Study

---
+++ CaseStudy/Controller/Index/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimonBejdak\CaseStudy\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class Index implements HttpGetActionInterface
+{
+    public function __construct(
+       public ScopeConfigInterface $scopeConfig,
+    ) {}
+
+    public function execute()
+    {
+        die($this->scopeConfig->getValue('configuration/general/greeting', ScopeInterface::SCOPE_STORE));
+    }
+}
